66c81f2fe5266a89e0428941198b559b0174e9e4
Satish <satish@editiorialistyx.com>
2020-12-14 17:05:19 +0530

    react-webcam face detect

---
+++ src/components/WebCamPicture.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Webcam from 'react-webcam';
+import '../main.scss';
+
+const videoConstraints = {
+  width: 350,
+  height: 350,
+  facingMode: 'user',
+};
+
+export default class WebCamPicure extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      takingPicture: false
+    }
+    this.image = null;
+    this.webcam = React.createRef();
+  }
+
+  capture = () => {
+    const imageSrc = this.webcam.current.getScreenshot();
+    //console.log("Take Picture");
+    this.props.landmarkPicture(imageSrc);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Webcam
+          audio={false}
+          height={350}
+          ref={this.webcam}
+          screenshotFormat="image/jpeg"
+          width={350}
+          videoConstraints={videoConstraints}
+        />
+        {/* <img src="/img/cameraIcon.png" alt="Take Pic" height={100}
+          
+        /> */}
+        <button type="button" class="btn btn-primary my-btn" onClick={this.capture}>Take Picture</button>
+      </div>
+    );
+  }
+}
